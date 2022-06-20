da2d224553cbdde14295ffb1074044a7e625362c
JordanRaiaNIU <z1887657@students.niu.edu>
2022-06-19 22:07:11 -0500

    added more comments to GenerateMatrix.js

--- src/Matrix/GenerateMatrix.js
+++ src/Matrix/GenerateMatrix.js
@@ -7,7 +7,7 @@ function generate_matrix({ row }) {
 
     var matrix = Array.from(Array(row.length), () => Array(row.length).fill(0)); //matrix that will be made from row
 
-    matrix[0] = row;
+    matrix[0] = row;    //set the beginning of matrix to the row
 
     //set the diagonal values of matrix
     for (let i = 0; i < row.length; i++) {
@@ -18,7 +18,7 @@ function generate_matrix({ row }) {
     for (let i = 1; i < row.length; i++) {
         let difference = matrix[i][i] - matrix[0][i]; //find distance between this row and initial row
         for (let j = 0; j < row.length; j++) {
-            matrix[i][j] = mod(matrix[0][j]+difference, row.length);
+            matrix[i][j] = mod(matrix[0][j]+difference, row.length);        //find value of cell by subtracting the difference from the top row
         }
     }
 
