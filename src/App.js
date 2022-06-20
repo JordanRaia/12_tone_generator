abbcb6b6e7b4d815f5df97c1abec0bbb1281c4c7
JordanRaiaNIU <z1887657@students.niu.edu>
2022-06-19 20:25:18 -0500

    early matrix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Matrix from "./Matrix/Matrix";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    var matrix = [
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0],
+        [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1],
+        [3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2],
+        [4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3],
+        [5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4],
+        [6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5],
+        [7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6],
+        [8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7],
+        [9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8],
+        [10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    ];
+
+    return (
+        <div className="App">
+            <h1>Matrix</h1>
+            <Matrix matrix={matrix} />
+        </div>
+    );
 }
 
 export default App;
